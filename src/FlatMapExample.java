2394b6af64e8b30fcaecd441366bba6b4da870e3
Deven Bawale <dbawale@pdx.edu>
2017-05-13 19:50:50 -0700

    Added some notes on reduction and added reduction example to FlatMap

--- src/FlatMapExample.java
+++ src/FlatMapExample.java
@@ -1,10 +1,12 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class FlatMapExample {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ClassNotFoundException{
         List<Integer> list1 = Arrays.asList(1,2,3,4,5,6,7);
         List<Integer> list2 = Arrays.asList(2,4,6);
         List<Integer> list3 = Arrays.asList(3,5,7);
@@ -13,10 +15,23 @@ public class FlatMapExample {
 
         list.forEach(System.out::println);
 
-        list.stream()
-//                .map(l -> l.size())
-                .flatMap(l -> l.stream())
-                .forEach(System.out::println);
+        Optional<Integer> i = list.stream()
+                .map(List::size)
+                .max(Comparator.naturalOrder());
+//                .reduce(0, (a, b) -> a +b);
+//                .flatMap(l -> l.stream())
+//                .forEach(System.out::println);
 
+        i.ifPresent(System.out::println);
+
+        List<List<Integer>> emptyList = Arrays.asList();
+
+        Integer j = emptyList.stream()
+                .map(List::size)
+                .max(Comparator.naturalOrder())
+                .orElse(0);
+//                .orElseThrow(ClassNotFoundException::new);
+        System.out.println(j);
+//j
     }
 }
